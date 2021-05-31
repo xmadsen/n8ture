d9e9d4de3f101063262ddeec606110e33c4a448e
Xander Madsen <xander.madsen@gmail.com>
2021-05-30 21:25:37 -0400

    Lifeform can get and use calories

--- n8ture/lifeform.py
+++ n8ture/lifeform.py
@@ -1,4 +1,18 @@
 class Lifeform():
-    def __init__(self, calorie_level=100, location=(0, 0)):
-        self.calorie_level = calorie_level
+    def __init__(self, location=(0, 0), calorie_level=100, max_calorie_level=120, calorie_usage_rate=1.0):
         self.location = location
+        self.calorie_level = calorie_level
+        self.calorie_usage_rate = calorie_usage_rate
+        self.max_calorie_level = max_calorie_level
+
+    def update(self):
+        self.use_calories(self.calorie_usage_rate)
+
+    def use_calories(self, calories):
+        self.calorie_level -= calories
+
+    def get_calories(self, calories):
+        if self.calorie_level + calories > self.max_calorie_level:
+            self.calorie_level = self.max_calorie_level
+            return
+        self.calorie_level += calories
--- tests/test_lifeform.py
+++ tests/test_lifeform.py
@@ -13,4 +13,27 @@ def test_lifeform_inits_with_location_tuple():
     assert isinstance(lifeform.location, tuple)
 
 
-# def test_lifeform
+def test_lifeform_inits_with_calorie_usage_rate():
+    lifeform = Lifeform()
+    assert isinstance(lifeform.calorie_usage_rate, float)
+
+
+def test_lifeform_subtracts_calorie_usage_rate_from_calorie_level_on_update():
+    lifeform = Lifeform()
+    before_level = lifeform.calorie_level
+    lifeform.update()
+    assert lifeform.calorie_level == before_level - lifeform.calorie_usage_rate
+
+
+def test_lifeform_gains_calories_on_get_calories():
+    lifeform = Lifeform()
+    before_level = lifeform.calorie_level
+    lifeform.get_calories(10)
+    assert lifeform.calorie_level == before_level + 10
+
+
+def test_lifeform_get_calories_will_not_gain_more_than_max_calorie_level():
+    lifeform = Lifeform(calorie_level=140, max_calorie_level=150)
+    more_calories_than_needed = 20
+    lifeform.get_calories(more_calories_than_needed)
+    assert lifeform.calorie_level == lifeform.max_calorie_level
